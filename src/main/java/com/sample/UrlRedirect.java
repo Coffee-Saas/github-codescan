619fd93df1d723580f9cd4bb87b47d00235a0406
moigupta <82052934+moigupta@users.noreply.github.com>
2021-08-06 10:51:35 -0700

    Update UrlRedirect.java

--- src/main/java/com/sample/UrlRedirect.java
+++ src/main/java/com/sample/UrlRedirect.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 public class UrlRedirect extends HttpServlet {
     private static final String VALID_REDIRECT = "http://cwe.mitre.org/data/definitions/601.html";
 
+    private static final String MOI_TEST = "test_string"
+        
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // BAD: a request parameter is incorporated without validation into a URL redirect
